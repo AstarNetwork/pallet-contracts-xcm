d9152959d48ae05478d91aa6fb8b282b09e6366d
Ashutosh Varma <ashutoshvarma11@live.com>
2023-03-09 11:22:40 +0530

    feat: update contract

--- ink/lib.rs
+++ ink/lib.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_env::{chain_extension::FromStatusCode, Environment};
-use ink_lang as ink;
-use scale::Decode;
+use ink::env::{chain_extension::FromStatusCode, Environment};
+use scale::{Decode, Encode};
 pub use xcm::{VersionedMultiAsset, VersionedMultiLocation, VersionedResponse, VersionedXcm};
 
-#[derive(Decode)]
+#[derive(Decode, Encode)]
 pub enum Error {
     NoResponse = 1,
+    ScaleError = 2,
 }
 
 impl FromStatusCode for Error {
@@ -20,119 +20,106 @@ impl FromStatusCode for Error {
     }
 }
 
+impl From<scale::Error> for Error {
+    fn from(_value: scale::Error) -> Self {
+        Self::ScaleError
+    }
+}
+
 #[ink::chain_extension]
 pub trait XCMExtension {
     type ErrorCode = Error;
 
-    #[ink(extension = 0x00010000, handle_status = false, returns_result = false)]
+    #[ink(extension = 0x00A0000, handle_status = false)]
     fn prepare_execute(xcm: VersionedXcm<()>) -> u64;
 
-    #[ink(extension = 0x00010001, handle_status = false, returns_result = false)]
+    #[ink(extension = 0x00A0001, handle_status = false)]
     fn execute();
 
-    #[ink(extension = 0x00010002, handle_status = false, returns_result = false)]
+    #[ink(extension = 0x00A0002, handle_status = false)]
     fn prepare_send(dest: VersionedMultiLocation, xcm: VersionedXcm<()>) -> VersionedMultiAsset;
 
-    #[ink(extension = 0x00010003, handle_status = false, returns_result = false)]
+    #[ink(extension = 0x00A0003, handle_status = false)]
     fn send();
 
-    #[ink(extension = 0x00010004, handle_status = false, returns_result = false)]
+    #[ink(extension = 0x00A0004, handle_status = false)]
     fn new_query() -> u64;
 
-    #[ink(extension = 0x00010005, handle_status = true, returns_result = false)]
+    #[ink(extension = 0x00A0005, handle_status = true)]
     fn take_response(query_id: u64) -> Result<VersionedResponse, Error>;
 }
 
 pub enum CustomEnvironment {}
 
 impl Environment for CustomEnvironment {
-    const MAX_EVENT_TOPICS: usize = <ink_env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
+    const MAX_EVENT_TOPICS: usize = <ink::env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
 
-    type AccountId = <ink_env::DefaultEnvironment as Environment>::AccountId;
-    type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
-    type Hash = <ink_env::DefaultEnvironment as Environment>::Hash;
-    type BlockNumber = <ink_env::DefaultEnvironment as Environment>::BlockNumber;
-    type Timestamp = <ink_env::DefaultEnvironment as Environment>::Timestamp;
+    type AccountId = <ink::env::DefaultEnvironment as Environment>::AccountId;
+    type Balance = <ink::env::DefaultEnvironment as Environment>::Balance;
+    type Hash = <ink::env::DefaultEnvironment as Environment>::Hash;
+    type BlockNumber = <ink::env::DefaultEnvironment as Environment>::BlockNumber;
+    type Timestamp = <ink::env::DefaultEnvironment as Environment>::Timestamp;
 
     type ChainExtension = XCMExtension;
 }
 
 #[ink::contract(env = crate::CustomEnvironment)]
 mod xcm_contract_poc {
-    use ink_env::call::Call;
+    use ink::prelude::vec::Vec;
+    use scale::Encode;
     pub use xcm::opaque::latest::prelude::{
         Junction, Junctions::X1, MultiLocation, NetworkId::Any, OriginKind, Transact, Xcm, *,
     };
-    //pub use xcm::opaque::latest::prelude::*;
-    use ink_prelude::vec::Vec;
     pub use xcm::{VersionedMultiAsset, VersionedMultiLocation, VersionedResponse, VersionedXcm};
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+
     #[ink(storage)]
-    pub struct XcmContractPoC {
-        value: bool,
-    }
+    #[derive(Default)]
+    pub struct XcmContractPoC;
 
     impl XcmContractPoC {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(value: bool) -> Self {
-            Self { value }
-        }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+            Default::default()
         }
 
         #[ink(message)]
-        pub fn prepare_message(&mut self, para_id: u32, call: Vec<u8>) {
-            let multi_location = VersionedMultiLocation::V1(MultiLocation {
-                parents: 1,
-                interior: X1(Junction::Parachain(para_id)),
-            });
-            let versioned_xcm: xcm::VersionedXcm<Call> = VersionedXcm::from(Xcm([
-                WithdrawAsset((Junctions::Here, 100_000_000_000).into()),
-                BuyExecution {
-                    fees: (Here, 100_000_000_000).into(),
-                    weight_limit: Unlimited
-                },
-                Transact {
-                    origin_type: OriginKind::SovereignAccount,
-                    require_weight_at_most: 1_000_000_000 as u64,
-                    call: call.into(),
-                },
-            ]
-                .to_vec()));
+        pub fn send_message(
+            &mut self,
+            para_id: u32,
+            call: Vec<u8>,
+            max_fees: Option<u128>,
+            max_weight: Option<u64>,
+        ) {
+            let caller: [u8; 32] = self.env().caller().encode().try_into().unwrap();
+            let max_weight = max_weight.unwrap_or(1_000_000_000);
+            let max_fees = max_fees.unwrap_or(100_000_000_000_000_000);
 
-            // self.env().extension().prepare_send(multi_location, versioned_xcm);
-        }
-
-        #[ink(message)]
-        pub fn send_message(&mut self, para_id: u32, call: Vec<u8>) {
             let multi_location = VersionedMultiLocation::V1(MultiLocation {
                 parents: 1,
                 interior: X1(Junction::Parachain(para_id)),
             });
             let versioned_xcm = VersionedXcm::from(Xcm([
-                WithdrawAsset((Junctions::Here, 100_000_000_000).into()),
+                DescendOrigin(Junctions::X1(Junction::AccountId32 {
+                    network: Kusama,
+                    id: caller,
+                })),
+                WithdrawAsset((Junctions::Here, max_fees).into()),
                 BuyExecution {
-                    fees: (Here, 100_000_000_000).into(),
-                    weight_limit: Unlimited
+                    fees: (Here, max_fees).into(),
+                    weight_limit: Unlimited,
                 },
                 Transact {
                     origin_type: OriginKind::SovereignAccount,
-                    require_weight_at_most: 1_000_000_000 as u64,
+                    require_weight_at_most: max_weight,
                     call: call.into(),
                 },
+                RefundSurplus,
             ]
-                .to_vec()));
+            .to_vec()));
 
-            self.env().extension().prepare_send(multi_location, versioned_xcm);
+            self.env()
+                .extension()
+                .prepare_send(multi_location, versioned_xcm);
             self.env().extension().send();
         }
     }
